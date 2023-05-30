dd5d5044f3be9ee7542eb02e2700b74f44dda7e0
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-30 23:09:33 +0200

    [FEAT]lavoro stampa messagio dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // creo una funzione per creare numeri randomici
 function generateRandomNumber (min,max) {
     return Math.floor(Math.random() * (max - min +1) ) + min;
@@ -36,17 +37,26 @@ for (let i=0; i<5; i++){
 
 
 // setto la funzione temporale per far sparire i numeri NB PER TEST A 3 SECONDI DA CAMBIARE
-setTimeout(GameNumber, 3000);
+setTimeout(GameNumber, 30000);
 function GameNumber() {
     printNumber.classList.add('none')  
 }
 
 // setto la funzione temporale per i prompt
-setTimeout(question, 4000);
+setTimeout(question, 31000);
+
+
 function question(){
     // creo il secondo array vuoto
+    let report= document.querySelector('.answer')
+console.log(report)
     let user_number = []
     let versus = false
+    let numberFinder =''
+    let final_number= []
+        console.log(final_number)
+    
+   
     for (let i=0; i<5; i++){
         let question = parseInt(prompt('Che numero hai visto?'))
         console.log(question)
@@ -55,22 +65,32 @@ function question(){
         //    creo una condizione per confrontare i due numeri
         if(random_number.includes(user_number[i])){
             versus = true
-                
+                numberFinder +=`<div> ${user_number[i]} </div>`
+                final_number.push(user_number[i])
+                console.log(numberFinder)
                 console.log(`hai indovinato il numero ${user_number[i]}`)
+                
 
         } else {
                 console.log('non hai indovinato')
             }
         }
+
         
-        // primo messaggio di risultato (da sistemare)
+
         if (versus === true) {
             console.log (`Hai indovinato `);
-            document.getElementById('answer').innerText= 'Hai indovinato!'
-        } else {
-            document.getElementById('answer').innerText= 'Non hai indovinato!'
+            report.innerHTML = `Bravo! Ti sei ricordato ${final_number.length} numeri: ${numberFinder}!`
+        console.log(report)
 
+            
+        } else {
+            report.innerHTML = 'Non ti sei ricordato nessun numero!'
+            
         }
+        
+        
+
            
 
         console.log(user_number)
